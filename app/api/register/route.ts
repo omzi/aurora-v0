c6fb5d3dec7fca88ce81da6cec0327afc63a160d
omzi <obiohaomezibe@gmail.com>
2023-11-21 15:40:41 +0100

    fix: added log to `/register` route

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -22,6 +22,7 @@ const POST = async (request: Request) => {
     // If everything goes well, return a success response
     return NextResponse.json({ message: 'User registered successfully!', user }, { status: 201 });
   } catch (error) {
+    console.log('Registration Error :>>', error);
     if (error instanceof z.ZodError) {
       // Return validation error messages
       return NextResponse.json({ message: 'A validation error occurred', errors: error.formErrors.fieldErrors }, { status: 400 });
