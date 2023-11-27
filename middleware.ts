ffc302edc43894ebef48c53b4c011517797301b2
omzi <obiohaomezibe@gmail.com>
2023-11-27 23:03:25 +0100

    fix: resolved middleware issue

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,26 @@
-import { withAuth } from 'next-auth/middleware';
+import { getToken } from 'next-auth/jwt';
+import { NextRequest, NextResponse } from 'next/server';
 
-export default withAuth({ });
+export const middleware = async (request: NextRequest) => {
+  const publicRoutes = ['/invoices/view'];
+  const token = await getToken({ req: request });
+	const isRootRoute = request.nextUrl.pathname === '/';
+  const excludePattern = /^\/(?!api|auth|api\/register|_next\/static|_next\/image|images|assets|favicon.ico).*/;
+  
+  if (!excludePattern.test(request.nextUrl.pathname) || isRootRoute) {
+    return NextResponse.next();
+  }
 
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api/public (public API routes)
-     * - auth (auth routes)
-     * - / (home route)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    '/((?!api/public|api/register|auth|_next/static|_next/image|images|assets|favicon.ico).*)'
-  ],
-};
+  if (publicRoutes.includes(request.nextUrl.pathname)) {
+    return NextResponse.next();
+  }
+
+  if (!token) {
+    const url = request.nextUrl.clone();
+		url.searchParams.set('callbackUrl', url.pathname);
+    url.pathname = '/auth/sign-in';
+    return NextResponse.redirect(url);
+  }
+  
+  return NextResponse.next();
+}
