e0d4d537a600b8c68785f665f97653919c7b719f
Tito <107757681+pappitito@users.noreply.github.com>
2023-11-27 22:29:01 +0100

    fixes and updates

---
+++ hooks/useDashboard.tsx
@@ -0,0 +1,37 @@
+import { useUserContext } from '#/components/contexts/UserContext'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import React from 'react'
+import { toast } from 'react-toastify'
+
+function useDashboard() {
+    const {selectedBusiness} = useUserContext()
+
+    const analyticsGetter = async()=>{
+       try {
+        const response = await axios.get(`/api/dashboardAnalytics?id=${selectedBusiness?.id}`)
+        return response.data.data        
+       } catch (error) {
+            toast.error('something went wrong')
+       }
+
+    }
+    const useGetAnalytics = ()=>{
+        const {data: analytics, isLoading: loadingAnalytics} = useQuery({
+            queryKey: ['analytics'],
+            queryFn: analyticsGetter
+        })
+        return {
+            analytics,
+            loadingAnalytics
+        }
+    }
+ 
+ 
+ 
+    return {
+        useGetAnalytics
+  }
+}
+
+export default useDashboard
