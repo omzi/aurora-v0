15f20b133fb2cdd0d9d44f18510fc6a905dd1c16
Kaempy <campa.banj@gmail.com>
2023-12-19 11:07:43 +0100

    fix: generated invoice success message

--- app/(main)/invoices/new/NewInvoice.tsx
+++ app/(main)/invoices/new/NewInvoice.tsx
@@ -1,26 +1,24 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
-/* eslint-disable no-mixed-spaces-and-tabs */
 'use client';
 
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
-import { addDays, format } from 'date-fns';
-import
-	{
-		ArrowLeftIcon,
-		CalendarIcon,
-		ChevronsUpDownIcon,
-		CogIcon,
-		PlusIcon,
-		SearchIcon,
-		XIcon
-	} from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import Loader from 'react-ts-loaders';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { addDays, format } from 'date-fns';
+import {
+	ArrowLeftIcon,
+	CalendarIcon,
+	ChevronsUpDownIcon,
+	CogIcon,
+	PlusIcon,
+	SearchIcon,
+	XIcon
+} from 'lucide-react';
 
 import { Customer, Invoice, SuccessResponse } from '#/common.types';
 import { useUserContext } from '#/components/contexts/UserContext';
@@ -28,46 +26,41 @@ import InvoiceItem from '#/components/InvoiceItem';
 import GeneratedInvoiceModal from '#/components/modals/GeneratedInvoiceModal';
 import { Button } from '#/components/ui/button';
 import { Calendar } from '#/components/ui/calendar';
-import
-	{
-		Card,
-		CardContent,
-		CardFooter,
-		CardHeader,
-		CardTitle
-	} from '#/components/ui/card';
-import
-	{
-		DropdownMenu,
-		DropdownMenuContent,
-		DropdownMenuGroup,
-		DropdownMenuItem,
-		DropdownMenuTrigger
-	} from '#/components/ui/dropdown-menu';
+import {
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
+	CardTitle
+} from '#/components/ui/card';
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuTrigger
+} from '#/components/ui/dropdown-menu';
 import { Input } from '#/components/ui/input';
-import
-	{
-		Popover,
-		PopoverContent,
-		PopoverTrigger
-	} from '#/components/ui/popover';
-import
-	{
-		Select,
-		SelectContent,
-		SelectItem,
-		SelectTrigger,
-		SelectValue
-	} from '#/components/ui/select';
-import
-	{
-		Table,
-		TableBody,
-		TableCell,
-		TableHead,
-		TableHeader,
-		TableRow
-	} from '#/components/ui/table';
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger
+} from '#/components/ui/popover';
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue
+} from '#/components/ui/select';
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow
+} from '#/components/ui/table';
 import { useCustomerModal } from '#/hooks/useCustomerModal';
 import { useInvoiceExit } from '#/hooks/useInvoiceExit';
 import getCustomers from '#/lib/actions/getCustomers';
@@ -106,13 +99,6 @@ const NewInvoice = () => {
     total > 0 ||
     customer ||
     date;
-	const hasUnsavedChanges =
-    description.trim() !== '' ||
-    quantity > 0 ||
-    price > 0 ||
-    total > 0 ||
-    customer ||
-    date;
 
 	useEffect(() => {
 		setTotal(quantity * price);
@@ -133,19 +119,9 @@ const NewInvoice = () => {
 				total
 			}
 		]);
-		setInvoiceItems([
-			...invoiceItems,
-			{
-				description,
-				price,
-				quantity,
-				total
-			}
-		]);
 		setOverallTotal(overallTotal + total);
 		toast.success('Item added successfully!');
 		resetInputs();
-	};
 	};
 
 	const resetInputs = async () => {
@@ -154,12 +130,10 @@ const NewInvoice = () => {
 		setQuantity(0);
 		setTotal(0);
 	};
-	};
 
 	const resetInvoice = async () => {
 		setInvoiceItems([]);
 		setOverallTotal(0);
-	};
 	};
 
 	const handleRemoveInvoiceItem = (idx: number) => {
@@ -170,15 +144,11 @@ const NewInvoice = () => {
 		setInvoiceItems(updatedInvoiceItems);
 		setOverallTotal(newTotalCost);
 	};
-	};
 
 	const { data: customers, isPending } = useQuery({
 		queryKey: ['userCustomers'],
 		queryFn: async () => {
 			try {
-				const response = (await getCustomers(
-          selectedBusiness!.id
-				)) as SuccessResponse<Customer[]>;
 				const response = (await getCustomers(
           selectedBusiness!.id
 				)) as SuccessResponse<Customer[]>;
@@ -196,16 +166,11 @@ const NewInvoice = () => {
 			customer.name.toLowerCase().includes(search.toLowerCase()) ||
       customer.email.toLowerCase().includes(search.toLowerCase())
 		);
-		return (
-			customer.name.toLowerCase().includes(search.toLowerCase()) ||
-      customer.email.toLowerCase().includes(search.toLowerCase())
-		);
 	});
 
 	const handleCustomerClick = (customer: Customer) => {
 		setSearch('');
 		setCustomer(customer);
-	};
 	};
 
 	const handleSubmit = async () => {
@@ -233,13 +198,7 @@ const NewInvoice = () => {
 			items: invoiceItems
 		};
 
-		};
-
 		try {
-			const response = await axios.post<{ data: Invoice }>(
-				`/api/invoices?businessId=${businessId}&customerId=${customerId}`,
-				data
-			);
 			const response = await axios.post<{ data: Invoice }>(
 				`/api/invoices?businessId=${businessId}&customerId=${customerId}`,
 				data
@@ -263,11 +222,9 @@ const NewInvoice = () => {
 			setIsSubmitting(false);
 		}
 	};
-	};
 
 	const handleClose = () => {
 		setShowGeneratedModal(false);
-	};
 	};
 
 	return (
@@ -277,53 +234,29 @@ const NewInvoice = () => {
 				isOpen={showGeneratedModal}
 				onOpenChange={handleClose}
 				appendedMessage={
-					customer?.name
-						? `Invoice generated successfully and sent to the email provided for ${customer.name}`
-						: undefined
+					customer?.name &&
+          `Invoice generated successfully and sent to the email provided for ${customer.name}`
 				}
 			/>
 			<div className="flex items-center justify-start gap-4 w-full">
-				{hasUnsavedChanges ? (
-					<Button variant="outline" size="icon" className="rounded-full">
+				<Button variant="outline" size="icon" className="rounded-full">
+					{hasUnsavedChanges ? (
 						<ArrowLeftIcon
 							className="cursor-pointer"
 							onClick={invoiceExit.onOpen}
 						/>
-					<Button variant="outline" size="icon" className="rounded-full">
-						<ArrowLeftIcon
-							className="cursor-pointer"
-							onClick={invoiceExit.onOpen}
-						/>
-					</Button>
-				) : (
-					<Link href="/invoices">
-						<Button variant="outline" size="icon" className="rounded-full">
-							<ArrowLeftIcon />
-						</Button>
-					<Link href="/invoices">
-						<Button variant="outline" size="icon" className="rounded-full">
-							<ArrowLeftIcon />
-						</Button>
-					</Link>
-				)}
-				<h1 className="text-2xl font-semibold">Create Invoice</h1>
+					) : (
+						<Link href="/invoices">
+							<ArrowLeftIcon className="cursor-pointer" />
+						</Link>
+					)}
+				</Button>
 				<h1 className="text-2xl font-semibold">Create Invoice</h1>
 			</div>
 			<div className="flex flex-col sm:flex-row items-center justify-between gap-4 my-4">
 				<div className="flex w-full  gap-3">
 					<DropdownMenu>
-			<div className="flex flex-col sm:flex-row items-center justify-between gap-4 my-4">
-				<div className="flex w-full  gap-3">
-					<DropdownMenu>
 						<DropdownMenuTrigger asChild>
-							<Button
-								variant="outline"
-								className="flex justify-between w-full sm:max-w-[300px]"
-							>
-								<span className="truncate">
-									{customer?.name || 'Select a customer'}
-								</span>
-								<ChevronsUpDownIcon className="w-3.5 h-3.5 ml-2" />
 							<Button
 								variant="outline"
 								className="flex justify-between w-full sm:max-w-[300px]"
@@ -340,55 +273,15 @@ const NewInvoice = () => {
 						>
 							<div className="relative m-0.5 mb-1">
 								<SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-zinc-500 dark:text-zinc-400" />
-						<DropdownMenuContent
-							align="start"
-							className="w-[300px] shadow-lg p-1"
-						>
-							<div className="relative m-0.5 mb-1">
-								<SearchIcon className="absolute left-2.5 top-2.5 h-4 w-4 text-zinc-500 dark:text-zinc-400" />
 								<Input
-									className="pl-8"
 									className="pl-8"
 									value={search}
 									onChange={e => setSearch(e.target.value)}
 									placeholder="Search customers..."
-									placeholder="Search customers..."
 								/>
 							</div>
 							<DropdownMenuGroup className="overflow-y-auto min-h-[52.5px] max-h-[250px] flex flex-col items-center">
-							<DropdownMenuGroup className="overflow-y-auto min-h-[52.5px] max-h-[250px] flex flex-col items-center">
 								{isPending ? (
-									<Loader
-										type="spinner"
-										size={36}
-										className="my-4 text-black dark:text-white leading-[0]"
-									/>
-								) : filteredCustomers && filteredCustomers.length > 0 ? (
-									filteredCustomers.map((customer, idx) => (
-										<DropdownMenuItem
-											className="cursor-pointer w-full"
-											key={idx}
-											onClick={() => handleCustomerClick(customer)}
-										>
-											<div className="flex flex-col items-start">
-												<div className="font-medium">{customer.name}</div>
-												<div className="text-zinc-500 dark:text-zinc-400">
-													{customer.email}
-												</div>
-											</div>
-										</DropdownMenuItem>
-									))
-								) : customers &&
-                  customers.length > 0 &&
-                  search.trim() !== '' ? (
-										<p className="my-4 text-xs text-center text-muted-foreground pb-2">
-                    No customers found with the search query ;(
-										</p>
-									) : (
-										<p className="my-4 text-xs text-center text-muted-foreground pb-2">
-                    No customers created yet ;(
-										</p>
-									)}
 									<Loader
 										type="spinner"
 										size={36}
@@ -433,16 +326,6 @@ const NewInvoice = () => {
 						</span>
 						<span className="hidden sm:flex md:hidden">New</span>
 						<span className="hidden md:flex">New Customer</span>
-					<Button
-						onClick={customerModal.onOpen}
-						type="button"
-						className="bg-core hover:bg-blue-800 text-white"
-					>
-						<span className="flex md:hidden">
-							<PlusIcon className="h-4 w-4 mr-0 sm:mr-2" />
-						</span>
-						<span className="hidden sm:flex md:hidden">New</span>
-						<span className="hidden md:flex">New Customer</span>
 					</Button>
 				</div>
 
@@ -450,13 +333,11 @@ const NewInvoice = () => {
 					<PopoverTrigger asChild>
 						<Button
 							variant="outline"
-							variant="outline"
 							className={cn(
 								'w-full sm:max-w-[250px] flex items-center justify-start text-left font-normal',
 								!date && 'text-muted-foreground'
 							)}
 						>
-							<CalendarIcon className="mr-2 h-4 w-4" />
 							<CalendarIcon className="mr-2 h-4 w-4" />
 							{date ? format(date, 'PPP') : <span>Pick a due date</span>}
 						</Button>
@@ -469,24 +350,9 @@ const NewInvoice = () => {
 							onValueChange={value => setDate(addDays(new Date(), parseInt(value)))
 							}
 						>
-					<PopoverContent
-						align="end"
-						className="flex w-auto flex-col space-y-2 p-2"
-					>
-						<Select
-							onValueChange={value => setDate(addDays(new Date(), parseInt(value)))
-							}
-						>
 							<SelectTrigger>
 								<SelectValue placeholder="Select" />
-								<SelectValue placeholder="Select" />
 							</SelectTrigger>
-							<SelectContent position="popper">
-								<SelectItem value="0">Today</SelectItem>
-								<SelectItem value="1">Tomorrow</SelectItem>
-								<SelectItem value="3">In 3 days</SelectItem>
-								<SelectItem value="7">In a week</SelectItem>
-								<SelectItem value="14">In 2 weeks</SelectItem>
 							<SelectContent position="popper">
 								<SelectItem value="0">Today</SelectItem>
 								<SelectItem value="1">Tomorrow</SelectItem>
@@ -497,13 +363,10 @@ const NewInvoice = () => {
 						</Select>
 						<div className="rounded-md border">
 							<Calendar mode="single" selected={date} onSelect={setDate} />
-						<div className="rounded-md border">
-							<Calendar mode="single" selected={date} onSelect={setDate} />
 						</div>
 					</PopoverContent>
 				</Popover>
 			</div>
-			<Card className="flex flex-col mt-4">
 			<Card className="flex flex-col mt-4">
 				<CardHeader>
 					<CardTitle>Client Information</CardTitle>
@@ -511,42 +374,25 @@ const NewInvoice = () => {
 				<CardContent>
 					<div className="grid grid-cols-1 gap-4 md:grid-cols-2">
 						<div className="space-y-2">
-					<div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-						<div className="space-y-2">
 							<p>Customer Name</p>
 							<p className="text-sm font-thin leading-none opacity-70">
 								{customer?.name || 'Not Available'}
 							</p>
-							<p className="text-sm font-thin leading-none opacity-70">
-								{customer?.name || 'Not Available'}
-							</p>
 						</div>
-						<div className="space-y-2">
 						<div className="space-y-2">
 							<p>Customer Email</p>
 							<p className="text-sm font-thin leading-none opacity-70">
 								{customer?.email || 'Not Available'}
 							</p>
-							<p className="text-sm font-thin leading-none opacity-70">
-								{customer?.email || 'Not Available'}
-							</p>
 						</div>
-						<div className="space-y-2">
 						<div className="space-y-2">
 							<p>Customer Phone </p>
 							<p className="text-sm font-thin leading-none opacity-70">
 								{customer?.phoneNumber || 'Not Available'}
 							</p>
-							<p className="text-sm font-thin leading-none opacity-70">
-								{customer?.phoneNumber || 'Not Available'}
-							</p>
 						</div>
 						<div className="space-y-2">
-						<div className="space-y-2">
 							<p>Customer Address</p>
-							<p className="text-sm font-thin leading-none opacity-70">
-								{customer?.address || 'Not Available'}
-							</p>
 							<p className="text-sm font-thin leading-none opacity-70">
 								{customer?.address || 'Not Available'}
 							</p>
@@ -555,22 +401,16 @@ const NewInvoice = () => {
 				</CardContent>
 			</Card>
 			<Card className="flex flex-col mt-4">
-			<Card className="flex flex-col mt-4">
 				<CardHeader>
 					<CardTitle>Invoice Items</CardTitle>
 				</CardHeader>
 				<CardContent className="flex gap-1">
 					<Table className="min-w-[30rem]">
-				<CardContent className="flex gap-1">
-					<Table className="min-w-[30rem]">
 						<TableHeader>
-							<TableRow className="hover:bg-transparent">
-								<TableHead className="pl-0">Description</TableHead>
 							<TableRow className="hover:bg-transparent">
 								<TableHead className="pl-0">Description</TableHead>
 								<TableHead>Quantity</TableHead>
 								<TableHead>Price (₦)</TableHead>
-								<TableHead className="text-right">Total (₦)</TableHead>
 								<TableHead className="text-right">Total (₦)</TableHead>
 							</TableRow>
 						</TableHeader>
@@ -595,51 +435,17 @@ const NewInvoice = () => {
 										value={description}
 										onChange={e => setDescription(e.target.value)}
 									/>
-							{invoiceItems.length > 0 &&
-                invoiceItems.map((invoice, idx) => (
-                	<InvoiceItem
-                		key={idx}
-                		description={invoice.description}
-                		quantity={invoice.quantity}
-                		price={formatNumberWithCommas(invoice.price)}
-                		total={formatNumberWithCommas(invoice.total)}
-                		onRemove={() => handleRemoveInvoiceItem(idx)}
-                	/>
-                ))}
-							<TableRow className="hover:bg-transparent">
-								<TableCell className="space-y-2 pl-0">
-									<Input
-										id="description"
-										type="text"
-										placeholder="Item description"
-										value={description}
-										onChange={e => setDescription(e.target.value)}
-									/>
 								</TableCell>
 								<TableCell className="space-y-2">
-								<TableCell className="space-y-2">
 									<Input
-										id="quantity"
-										placeholder="Quantity"
-										type="number"
 										id="quantity"
 										placeholder="Quantity"
 										type="number"
 										value={quantity}
 										onChange={e => setQuantity(Math.max(0, e.target.valueAsNumber))
 										}
-										onChange={e => setQuantity(Math.max(0, e.target.valueAsNumber))
-										}
 									/>
 								</TableCell>
-								<TableCell className="space-y-2">
-									<Input
-										id="price"
-										placeholder="Price"
-										type="number"
-										value={price}
-										onChange={e => setPrice(Math.max(0, e.target.valueAsNumber))
-										}
 								<TableCell className="space-y-2">
 									<Input
 										id="price"
@@ -658,21 +464,7 @@ const NewInvoice = () => {
 										value={total}
 										disabled
 									/>
-								<TableCell className="space-y-2">
-									<Input
-										id="total"
-										placeholder="Total"
-										type="number"
-										value={total}
-										disabled
-									/>
 								</TableCell>
-
-								<TableCell className="!p-1 w-4 h-4 cursor-pointer rounded-sm bg-white/10 hover:bg-white/25">
-									<XIcon
-										onClick={resetInputs}
-										className="w-4 h-4 text-black cursor-pointer"
-									/>
 
 								<TableCell className="!p-1 w-4 h-4 cursor-pointer rounded-sm bg-white/10 hover:bg-white/25">
 									<XIcon
@@ -699,24 +491,8 @@ const NewInvoice = () => {
 					>
             Add Item
 					</Button>
-				<CardFooter className="flex items-center justify-between">
-					<Button
-						onClick={resetInvoice}
-						size="sm"
-						className="text-white bg-red-600 hover:bg-red-700"
-					>
-            Clear Invoice
-					</Button>
-					<Button
-						onClick={onAdd}
-						size="sm"
-						className="text-white bg-core hover:bg-blue-800"
-					>
-            Add Item
-					</Button>
 				</CardFooter>
 			</Card>
-			<Card className="flex flex-col mt-4 w-full sm:w-96 ml-auto">
 			<Card className="flex flex-col mt-4 w-full sm:w-96 ml-auto">
 				<CardHeader>
 					<CardTitle>Summary</CardTitle>
@@ -726,24 +502,15 @@ const NewInvoice = () => {
 					<span className="text-lg font-semibold">
 						{formatNumberWithCommas(overallTotal)}
 					</span>
-				<CardContent className="flex items-center justify-between">
-					<span className="mr-4">Total Amount (₦):</span>
-					<span className="text-lg font-semibold">
-						{formatNumberWithCommas(overallTotal)}
-					</span>
 				</CardContent>
 			</Card>
-			<div className="flex items-center pt-4 pb-8">
 			<div className="flex items-center pt-4 pb-8">
 				<Button
 					onClick={handleSubmit}
 					disabled={isSubmitting}
 					className="ml-auto relative bg-core hover:bg-blue-800 text-white"
-					className="ml-auto relative bg-core hover:bg-blue-800 text-white"
 				>
 					{isSubmitting ? (
-						<span className="opacity-0 flex items-center">
-							<CogIcon className="h-4 w-4 mr-2" />
 						<span className="opacity-0 flex items-center">
 							<CogIcon className="h-4 w-4 mr-2" />
               Generate Invoice
@@ -751,19 +518,14 @@ const NewInvoice = () => {
 					) : (
 						<span className="opacity-100 flex items-center">
 							<CogIcon className="h-4 w-4 mr-2" />
-						<span className="opacity-100 flex items-center">
-							<CogIcon className="h-4 w-4 mr-2" />
               Generate Invoice
 						</span>
 					)}
 					{isSubmitting && (
 						<div className="absolute flex items-center justify-center w-full h-full">
-						<div className="absolute flex items-center justify-center w-full h-full">
 							<Loader
 								type="spinner"
-								type="spinner"
 								size={28}
-								className="text-black dark:text-white leading-[0]"
 								className="text-black dark:text-white leading-[0]"
 							/>
 						</div>
@@ -771,8 +533,6 @@ const NewInvoice = () => {
 				</Button>
 			</div>
 		</div>
-	);
-};
 	);
 };
 
